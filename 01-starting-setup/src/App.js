8bf5971e5c966f7300def800742278ff23e0e0f5
coderballer <shhchen122@gmail.com>
2022-04-28 22:51:16 -0400

    added ExpenseItem component. close #1

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started! - Go!</h2>
+      <p>GO! Go! Go!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
